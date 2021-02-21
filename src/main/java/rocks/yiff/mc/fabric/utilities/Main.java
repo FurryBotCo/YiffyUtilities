19ffa4f6aed92bde3baa6c48830348718e51d395
Donovan Daniels <dwddaniels262@gmail.com>
2021-02-21 14:40:00 -0600

    Initial Commit

---
+++ src/main/java/rocks/yiff/mc/fabric/utilities/Main.java
@@ -0,0 +1,10 @@
+package rocks.yiff.mc.fabric.utilities;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Main implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
